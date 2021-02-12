ddf75761c8d5ad0570b09b26163cb94fe4efab7a
Budgo89 <77630656+Budgo89@users.noreply.github.com>
2021-02-12 19:50:15 +0300

    Add files via upload

---
+++ DZ Lesson 1/DZ Lesson 1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ_Lesson_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Сапунов Максим";
+            DateTime date = new DateTime(2021, 2, 12, 16, 15, 32);
+            Console.WriteLine($"Привет, {name}, сегодня Data {date.ToString("dd.MM.yy")}");
+            Console.ReadLine();
+        }
+    }
+}
